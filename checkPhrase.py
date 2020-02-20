87107e7c060ceaf5fc34b311be65610edaef7811
Jacek Wyrostek <42946180+jacekwyrostek@users.noreply.github.com>
2020-02-20 02:04:10 +0100

    Create checkPhrase.py

---
+++ checkPhrase.py
@@ -0,0 +1,18 @@
+def checkPhrase(txtFile)
+  list=open(txtFile,'r')
+  x=0
+  for line in list:
+      duplicates=False
+      line=line.rstrip()
+      print(line)
+      line2=line.split(' ')
+      count=0
+      for word in line2:
+          count=line2.count(word)
+          if count > 1:
+              duplicates=True
+      if duplicates:
+          x+=1
+      print('duplicate='+str(duplicates))
+      print('='*20)
+  print(x)
